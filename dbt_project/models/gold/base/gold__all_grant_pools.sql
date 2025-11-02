da3c6571e95a54370b60f7afc5ddf1e4641628e9
Rashmi V Abbigeri <rashmiabbigeri@gmail.com>
2025-11-03 02:24:14 +0530

    Gold setup and models working

--- dbt_project/models/gold/base/gold__all_grant_pools.sql
+++ dbt_project/models/gold/base/gold__all_grant_pools.sql
@@ -1,4 +1,7 @@
 {{ config(materialized='view') }}
 
-SELECT 'giveth' AS source, * FROM {{ ref('silver_giveth_grant_pools') }}
+SELECT
+  'Giveth' AS source,
+  *
+FROM {{ source('silver', 'silver_giveth_grant_pools') }}
 -- UNION ALL other sources later (scf, celo, gitcoin, etc.)
