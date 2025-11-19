c18e4b48d41d83e5ef8caf15e5fd69d2c949ec5a
Rashmi V Abbigeri <rashmiabbigeri@gmail.com>
2025-11-20 02:24:13 +0530

    dbt gold run success

--- dbt_project/target/run/opengrants/models/gold/metrics/giveth/gold__engagement_metrics.sql
+++ dbt_project/target/run/opengrants/models/gold/metrics/giveth/gold__engagement_metrics.sql
@@ -13,7 +13,6 @@
 SELECT
     'giveth' as platform,
     COUNT(*) AS total_projects,
-    AVG("io.giveth.qualityScore") AS avg_quality_score,
     SUM("io.giveth.totalReactions") AS total_reactions,
     AVG("io.giveth.totalReactions") AS avg_reactions_per_project,
     COUNT(CASE WHEN "io.giveth.listed" = true THEN 1 END) AS listed_projects
