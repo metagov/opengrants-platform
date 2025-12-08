1b94e9464b800c521de7e375b4aad0a9791c8fcb
Rashmi V Abbigeri <rashmiabbigeri@gmail.com>
2025-12-08 14:44:01 +0530

    rearrange FE

--- nextjs-dashboard/src/lib/db.tsx
+++ nextjs-dashboard/src/lib/db.tsx
@@ -2,10 +2,9 @@
 import { Pool } from 'pg'
 
 const pool = new Pool({
-  connectionString: 'postgresql://postgres:postgres@0.tcp.eu.ngrok.io:16940/opengrants',
+  connectionString: process.env.DATABASE_URL,
 })
 
-//connectionString: 'postgresql://postgres:postgres@localhost:5433/opengrants',
 
 export async function query(text: any, params: any) {
   const client = await pool.connect()
